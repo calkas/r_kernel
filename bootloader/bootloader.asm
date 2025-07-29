206f9d6f81fb1b93d204a340a9693cf519bf8d6f
Piotr Rybka <piotr.rybka@nokia.com>
2025-07-29 14:10:04 +0200

    Working bootloader - jumps to os entry address 0x1000

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -3,9 +3,6 @@
 ; It switches to protected mode, loads the kernel from disk, and jumps to it.
 
 ; Memory layout after booting:
-;        | R_Kernel entry point             |
-;0x200000|----------------------------------|
-;        | free                             |
 ;0x100000|----------------------------------|
 ;        | BIOS (256 kB)                    |
 ; 0xC0000|----------------------------------|
@@ -27,66 +24,42 @@
 
 [org 0x7C00]
 [BITS 16]
-
+OS_OFFSET_ADDRESS equ 0x1000 ; Offset where the kernel will be loaded 0x7C00 + 0x1000 = 0x8C00
 start:
-    ; Set up the stack
-    mov ax, 0x9000
-    mov ss, ax
-    mov sp, 0xFFFF
-
-    ; Print welcome message
-    mov si, welcome_msg
-    call print
-
-    ; Load the kernel
-    call load_kernel
-
-    ; Switch to protected mode
-    call switch_to_protected_mode
-
-    ; This point is never reached
-    jmp $
-
-; Include GDT setup
-%include "gdt_32bit.asm"
-
-load_kernel:
-    ; Load the kernel from disk
-    mov ax, 0x0000  ; Segment where the kernel will be loaded
-    mov es, ax      ; Set ES to segment 0
-    mov bx, 0x1000  ; Load kernel at 0x1000:0000
-
-    ; Read the kernel from disk (assuming it is in the first sector)
-    mov ah, 0x02    ; BIOS read sector function
-    mov al, 15      ; N sectors to read
-    mov ch, 0       ; Cylinder 0
-    mov cl, 2       ; Sector 2 (skipping boot sector)
-    mov dh, 0       ; Head 0
-    int 0x13        ; Call BIOS interrupt
-
-    jc .load_error   ; Jump if carry flag is set (error)
-
-    ret
-
-
-.load_error:
-    ; Handle load error
-    mov si, error_msg
-    call print
-    jmp $
-switch_to_protected_mode:
     cli
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov sp, 0x7C00
+    ; Set up the stack
+    mov sp, 0x9000 ; 0x0000×16 + 0x9000 = 0x9000
+    sti
 
-    ; Enable A20 line
-    in al, 0x92
-    or al, 0x02
-    out 0x92, al
+    call load_kernel
+    call welcome_msg
+    call switch_to_protected_mode
 
+
+; Include GDT setup
+%include "gdt_32bit.asm"
+
+load_kernel:
+    ; Set OS entry: 0x0000:0x1000 = 0x1000
+    mov bx, OS_OFFSET_ADDRESS
+
+    ; INT 13h - read sector
+    mov ah, 0x02        ; Function: read
+    mov al, 1           ; Number of sectors
+    mov ch, 0           ; Cylinder
+    mov cl, 2           ; Sector (must be > 0)
+    mov dh, 0           ; Head
+    mov dl, 0x80        ; Hard disk
+    int 0x13
+    jc load_error       ; If error, jump to message
+    ret
+
+switch_to_protected_mode:
+    cli
     ; Load GDT
     lgdt [gdt_descriptor]
 
@@ -97,6 +70,31 @@ switch_to_protected_mode:
 
     ; Far jump to 32-bit code
     jmp CODE_SEGMENT:protected_mode_entry
+
+
+welcome_msg:
+    mov si, w_msg
+    call print
+    ret
+
+load_error:
+    mov si, error_msg
+    call print
+    jmp $
+
+print:
+    mov ah, 0x0E
+.next:
+    lodsb
+    or al, al
+    jz .done
+    int 0x10
+    jmp .next
+.done:
+    ret
+
+error_msg db "INT 13h load error!", 0
+w_msg db "Loading kernel...", 0
 
 ; -------------------------------
 ; Protected Mode
@@ -114,30 +112,13 @@ protected_mode_entry:
     mov esp, ebp
 
     ; Jump to the kernel entry point
-    jmp CODE_SEGMENT:0x100000
+    jmp CODE_SEGMENT:OS_OFFSET_ADDRESS
 
 .hang:
     cli
     hlt
     jmp .hang
 
-; -------------------------------
-; BIOS Print Function
-; -------------------------------
-print:
-    mov ah, 0x0E
-.next:
-    lodsb
-    cmp al, 0
-    je .done
-    int 0x10
-    jmp .next
-.done:
-    ret
-
-welcome_msg db "Load R_Kernel...", 0
-error_msg db "Error loading kernel!", 0
-
-; Boot sector padding
+; Fill 512 bytes - padding
 times 510 - ($ - $$) db 0
 dw 0xAA55
--- build.sh
+++ build.sh
@@ -20,11 +20,15 @@ fi
 mkdir -p "$OUTPUT_DIR"
 
 echo " -> Kernel compilation (release)"
+cargo clean
 cargo build --release --target "$RUST_TARGET.json"
 
 echo " -> Stripping and converting kernel to raw binary"
 rust-objcopy "target/$RUST_TARGET/release/$RUST_KERNEL" \
     --strip-all -O binary "$OUTPUT_DIR/kernel.bin"
+
+echo " -> Displaying entry kernel address"
+nm -C "target/$RUST_TARGET/release/$RUST_KERNEL" | grep -E '_start'
 
 echo " -> Bootloader compilation"
 nasm -f bin -I bootloader/ "$ASM_FILE" -o "$BIN_FILE"
@@ -43,11 +47,18 @@ cp "$IMG_FILE" "$OUTPUT_DIR/"
 rm -f "$BIN_FILE" "$IMG_FILE"
 
 echo " -> Merging bootloader and kernel"
+#truncate -s 512 "$OUTPUT_DIR/kernel.bin"
 cat "$OUTPUT_DIR/bootloader.bin" "$OUTPUT_DIR/kernel.bin" > "$OUTPUT_DIR/r_os.bin"
 
 #calculate number of sectors
+BOOT_SIZE=$(stat -c%s "$OUTPUT_DIR/bootloader.bin")
+echo " * Bootloader size: $BOOT_SIZE bytes"
+
 KERNEL_SIZE=$(stat -c%s "$OUTPUT_DIR/kernel.bin")
 SECTORS=$(( (KERNEL_SIZE + 511) / 512 ))
-echo " -> Kernel size: $KERNEL_SIZE bytes ($SECTORS sectors)"
+echo " * Kernel size: $KERNEL_SIZE bytes ($SECTORS sectors)"
+
+OS_SIZE=$(stat -c%s "$OUTPUT_DIR/r_os.bin")
+echo " * OS size: $OS_SIZE bytes"
 
 echo "==================== Done ===================="
